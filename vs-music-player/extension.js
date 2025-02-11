d01b45044ef66d2211e5da22a42cae020d08b61e
KOWSIK-M <2200030358@kluniversity.in>
2025-02-11 22:52:04 +0530

    small changes

--- vs-music-player/extension.js
+++ vs-music-player/extension.js
@@ -378,7 +378,6 @@ function activate(context) {
                 <div class="card">
                     
                     <div class="card__title" id="song-name">${songQueue[currentSongIndex].name}</div>
-                    <div class="card__subtitle">Smalltown Boy , Shane D</div>
                     <div class="card__wrapper">
                         <div class="card__time card__time-passed" id="progress-text" >0:00</div>
                         <div class="card__timeline"><progress id="progress-bar" value="0" max="100"></progress></div>
@@ -393,6 +392,22 @@ function activate(context) {
                     </div>
                     <div style="display:flex;"> 
                         <p><i class="fas fa-music"></i> Auto Play : </p><button id="autoPlayToggle"style="border-radius:6px;"> Enable</button>
+                        <p style="height:32px;width:260px;margin-left:800px;font-size:16px;">Support me at &nbsp;&nbsp;&nbsp;
+                        <a href="https://www.linkedin.com/in/medam-kowsik-975479282/">
+                        <svg style="height:32px;width:32px;" fill="#fff" aria-label="LinkedIn" viewBox="0 0 512 512" id="linkedin">
+                            <rect width="512" height="512" fill="#0077b5" rx="15%"></rect>
+                            <circle cx="142" cy="138" r="37"></circle>
+                            <path stroke="#fff" stroke-width="66" d="M244 194v198M142 194v198"></path>
+                            <path d="M276 282c0-20 13-40 36-40 24 0 33 18 33 45v105h66V279c0-61-32-89-76-89-34 0-51 19-59 32"></path>
+                          </svg>
+                          </a>
+                          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                          <a href="https://github.com/KOWSIK-M" >
+                          <svg viewBox="0 0 16 16" id="github" style="height:32px;width:32px;fill:white">
+                            <path d="M7.999 0C3.582 0 0 3.596 0 8.032a8.031 8.031 0 0 0 5.472 7.621c.4.074.546-.174.546-.387 0-.191-.007-.696-.011-1.366-2.225.485-2.695-1.077-2.695-1.077-.363-.928-.888-1.175-.888-1.175-.727-.498.054-.488.054-.488.803.057 1.225.828 1.225.828.714 1.227 1.873.873 2.329.667.072-.519.279-.873.508-1.074-1.776-.203-3.644-.892-3.644-3.969 0-.877.312-1.594.824-2.156-.083-.203-.357-1.02.078-2.125 0 0 .672-.216 2.2.823a7.633 7.633 0 0 1 2.003-.27 7.65 7.65 0 0 1 2.003.271c1.527-1.039 2.198-.823 2.198-.823.436 1.106.162 1.922.08 2.125.513.562.822 1.279.822 2.156 0 3.085-1.87 3.764-3.652 3.963.287.248.543.738.543 1.487 0 1.074-.01 1.94-.01 2.203 0 .215.144.465.55.386A8.032 8.032 0 0 0 16 8.032C16 3.596 12.418 0 7.999 0z"></path>
+                          </svg>
+                          </a>
+                        </p>
                     </div>
                 </div>
             </div>
